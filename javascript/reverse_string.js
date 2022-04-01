fcdec0d9c9c4c717fb24b94130fbaaa4b99c1c21
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-03-31 20:03:29 -0500

    commit tree c05cdd4a7e0fb37adce67debf1778b4360da4b7a

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,9 @@
 function reverseString(str) {
-  // type your code here
+  let revStr = ""
+  for(let i = str.length -1; i >= 0; i--){
+    revStr += str[i]
+  }
+  return revStr
 }
 
 if (require.main === module) {
@@ -17,3 +21,11 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+function reverseStr takes in parameter of string
+declare reversed string variable with "" so js knows its specifically a string
+for loop:
+  i starts at the end of the string's length (-1) and as long as it's greater than or equal to 0, it will decrement
+  i must be greater than or = to 0 because the index, if starting at end of string, will be greater than 0 (for example, in "hello" i starts at [4])
+return reversedStr VARIABLE + str[i]. We declared revStr as a blank str so it will just take on the reversed letters
+*/
